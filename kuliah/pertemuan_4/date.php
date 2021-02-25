9ab712579f051ae6e7001a72f012495c968718b2
Anam140601 <khoerulanam140601@gmail.com>
2021-02-25 14:49:49 +0700

    Fungsi

---
+++ kuliah/pertemuan_4/date.php
@@ -0,0 +1,33 @@
+<?php 
+    // date
+    // echo date("l, d-M-Y");
+
+
+    //time
+    // echo time();
+
+    // hitung 2 hari kedepan
+    // echo date("l",time()+170000);
+
+    // hitung 100 hari kedepan
+    // echo date("l",time()+60*60*24*120);
+
+    // hiutng 100 hari kebelakang
+    // echo date("l, d M Y",time()-60*60*24*120);
+
+
+    // mktime
+    // membuat sendiri detik dari 1 januari 1970
+    // mktime(0,0,0,0,0,0)
+    // mktime(jam, menit, detik, bulan, tanggal, tahun)
+
+    // $time = mktime(0,0,0,11,04,2001);
+    // echo $time;
+    // echo date("l",$time);
+
+
+    // strtotime
+    // $time = strtotime("14 may 2001");
+    // echo date("l", $time);
+
+?>
