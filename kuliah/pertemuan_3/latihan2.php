d1616fe16cc96ed5729415a53a28d71741e9e103
Anam140601 <khoerulanam140601@gmail.com>
2021-02-25 14:02:13 +0700

    Struktur Kendali, Pengulangan dan pengkondisian

---
+++ kuliah/pertemuan_3/latihan2.php
@@ -0,0 +1,34 @@
+<?php
+// pengkodisian
+
+// if
+// // $x=10;
+// $x = 20;
+// if ($x < 20) {
+//     echo "yes";
+// }
+
+
+// if else
+// $x=10;
+// // $x = 20;
+// if ($x < 20) {
+//     echo "yes";
+// } else {
+//     echo "no";
+// }
+
+
+// if elseif else
+// $x=20;
+// // $x = 20;
+// if ($x < 20) {
+//     echo "yes";
+// } elseif($x == 30) {
+//     echo "pas";
+// } 
+// else {
+//     echo "no";
+// }
+
+?>
